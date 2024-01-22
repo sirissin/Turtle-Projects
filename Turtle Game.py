4ee3531531d37c25369affd442abfaa621581baa
Siris <aperry2171@gmail.com>
2024-01-22 15:54:13 -0500

    Changed the shape of the turtle to a Turtle. Created the functions for movement of the Turtle. Mapped the keystrokes to the functions for movement.
    Turtle

--- Turtle Game.py
+++ Turtle Game.py
@@ -19,5 +19,40 @@ goto(300, 200)
 goto(300, -200)
 goto(100, -200)
 end_fill()
+penup()
 
+
+goto(-200, 0)
+color("green")
+shape("turtle")
+
+
+# Created the Functions for moving and rotating the Turtle
+def move_up():
+    setheading(90)
+    forward(move_distance)
+
+
+def move_down():
+    setheading(270)
+    forward(move_distance)
+
+
+def move_left():
+    setheading(180)
+    forward(move_distance)
+
+
+def move_right():
+    setheading(0)
+    forward(move_distance)
+
+
+# Functionality for moving the Turtle by pressing keys
+onkey(move_up, key="Up")
+onkey(move_down, key="Down")
+onkey(move_left, key="Left")
+onkey(move_right, key="Right")
+
+listen()
 done()
