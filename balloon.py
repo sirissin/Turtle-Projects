9c8f41ddddbba315df4e81de830433656e80e26e
Siris <aperry2171@gmail.com>
2024-01-22 12:29:58 -0500

    Inflation now require the user to press the "Up" arrow key. The balloon will now pop when the set limit is reached and print text to represent the pop.

--- balloon.py
+++ balloon.py
@@ -4,18 +4,30 @@ from turtle import *
 diameter = 40
 pop_diameter = 100
 
+
 #Draw the balloon
 def draw_balloon():
     color("red")
     dot(diameter)
 
 
+#Inflate the Balloon
 def inflate_balloon():
     global diameter
     diameter = diameter + 10
     draw_balloon()
 
+    #Limit the balloon inflation and cause the Pop
+    if diameter >= pop_diameter:
+        clear()
+        diameter = 40
+        write('POP!')
+
 
 draw_balloon()
-inflate_balloon()
+
+#Use the up arrow key to inflate the balloon
+onkey(inflate_balloon, "Up")
+listen()
+
 Screen().exitonclick()
