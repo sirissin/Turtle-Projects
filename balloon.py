834cfca075549c00820ee36ddf6dc0bd9f252463
Siris <aperry2171@gmail.com>
2024-01-22 12:18:33 -0500

    Added a basic function for inflation.

--- balloon.py
+++ balloon.py
@@ -1,13 +1,21 @@
 from turtle import *
 
-
+#Balloon parameters
 diameter = 40
 pop_diameter = 100
 
+#Draw the balloon
 def draw_balloon():
     color("red")
     dot(diameter)
 
 
+def inflate_balloon():
+    global diameter
+    diameter = diameter + 10
+    draw_balloon()
+
+
 draw_balloon()
+inflate_balloon()
 Screen().exitonclick()
